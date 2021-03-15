563e4f5ac19f2689078d8f4c3c555ad10d3aba1c
microram <jeff@micro-ram.com>
2021-03-15 11:48:10 -0500

    Added direct-printing to deal with KB5000802 KB500808

---
+++ printer/direct-printing.ps1
@@ -0,0 +1,6 @@
+$Printers = Get-Printer
+foreach ($Printer in $Printers){
+    $cmd = 'printui.dll,PrintUIEntry /Xs /n "' + $Printer.Name + '" attributes +direct'
+    Write-Host $cmd
+    Start-Process -Filepath C:\Windows\System32\rundll32.exe -ArgumentList $cmd -WorkingDirectory C:\Windows\System32
+}
