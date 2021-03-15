b77078ef6d0060d6022dec8bb49ded93dafee361
microram <jeff@micro-ram.com>
2021-03-15 12:00:39 -0500

    updated with undo note

--- printer/direct-printing.ps1
+++ printer/direct-printing.ps1
@@ -1,3 +1,4 @@
+## Loop through all printers and set to direct printing (-direct to reverse)
 $Printers = Get-Printer
 foreach ($Printer in $Printers){
     $cmd = 'printui.dll,PrintUIEntry /Xs /n "' + $Printer.Name + '" attributes +direct'
