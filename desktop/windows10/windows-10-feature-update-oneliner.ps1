1b4b26de10574956be53fbf2627d3df0eda692ab
microram <jeff@micro-ram.com>
2021-03-17 15:45:21 -0500

    Testing new oneliner

--- desktop/windows10/windows-10-feature-update-oneliner.ps1
+++ desktop/windows10/windows-10-feature-update-oneliner.ps1
@@ -1,1 +1,3 @@
-Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkID=799445 -OutFile $env:temp\Win10Upgrade.exe; Start-Process -FilePath $env:temp\Win10Upgrade.exe -ArgumentList "/quietinstall /skipeula /auto upgrade /copylogs $($env:temp)"
+IWR https://go.microsoft.com/fwlink/?LinkID=799445 -OutFile $env:temp\wu.exe; Start-Process $env:temp\wu.exe -ArgumentList "/QuietInstall /skipeula /auto upgrade /Finalize"
+
+#Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkID=799445 -OutFile $env:temp\Win10Upgrade.exe; Start-Process -FilePath $env:temp\Win10Upgrade.exe -ArgumentList "/quietinstall /skipeula /auto upgrade /copylogs $($env:temp)"
