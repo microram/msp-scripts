b0cd84ae43e5c48b59f944bb0b86657ce275399f
microram <jeff@micro-ram.com>
2021-03-17 15:01:41 -0500

    Added Feature-Ugrade oneliner

---
+++ desktop/windows10/windows-10-feature-upgrade-oneliner.ps1
@@ -0,0 +1,1 @@
+Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkID=799445 -OutFile $env:temp\Win10Upgrade.exe; Start-Process -FilePath $env:temp\Win10Upgrade.exe -ArgumentList "/quietinstall /skipeula /auto upgrade /copylogs $($env:temp)"
